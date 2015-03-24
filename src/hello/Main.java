8ca393153c9492313d3f8e0c1d472c7e4bf18bf7
Anna Slyvka <aslyvka@ASlivka.kiev.luxoft.com>
2015-03-24 14:32:28 +0200

    added today date.

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,10 +1,14 @@
 package hello;
 
+import java.util.Date;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("I'm changed!");
 		System.out.println("I'm changed again!");
+		System.out.println("Today date is " + new Date());
+		
 	}
 }
