f9f924df58e4a66cce0202339e3912f8e7449c94
Anna Slyvka <aslyvka@ASlivka.kiev.luxoft.com>
2015-03-24 14:43:14 +0200

    added new msg life is good

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("I'm changed!");
-		System.out.println("I'm changed again!");
+		System.out.println("I'm changed again!");		
+		System.out.println("Life is good!!!!!");
 	}
 }
